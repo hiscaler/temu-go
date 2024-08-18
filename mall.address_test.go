928493ebcad20836f8325c556f5c0a0894342d68
hiscaler <hiscaler@gmail.com>
2024-08-18 11:56:45 +0800

    ...

---
+++ mall.address_test.go
@@ -0,0 +1,19 @@
+package temu
+
+import (
+	"github.com/hiscaler/temu-go/entity"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_mallAddressService_All(t *testing.T) {
+	addresses, err := temuClient.Services.MallAddressService.All()
+	assert.Equal(t, nil, err, "Test_mallAddressService_All")
+
+	for _, address := range addresses {
+		var addr entity.MallAddress
+		addr, err = temuClient.Services.MallAddressService.One(address.ID)
+		assert.Equal(t, nil, err, "Services.MallAddressService.One(%d)", address.ID)
+		assert.Equal(t, addr, address, "Services.MallAddressService.One(%d)", address.ID)
+	}
+}
