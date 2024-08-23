dc1bd81f73928c38023653967224ba8975f9be1a
hiscaler <hiscaler@gmail.com>
2024-08-23 19:32:37 +0800

    ...

--- entity/mall.address.go
+++ entity/mall.address.go
@@ -2,15 +2,15 @@ package entity
 
 // MallAddress 卖家发货地址
 type MallAddress struct {
-	ID           int    `json:"id"`
-	IsDefault    bool   `json:"isDefault"`
-	DistrictCode int    `json:"districtCode"`
-	Address      string `json:"addressDetail"`
-	CityName     string `json:"cityName"`
-	DistrictName string `json:"districtName"`
-	MallID       int    `json:"mallId"`
-	ProvinceCode int    `json:"provinceCode"`
-	CityCode     int    `json:"cityCode"`
-	ProvinceName string `json:"provinceName"`
-	AddressLabel string `json:"addressLabel"`
+	ID            int    `json:"id"`
+	IsDefault     bool   `json:"isDefault"`
+	DistrictCode  int    `json:"districtCode"`
+	AddressDetail string `json:"addressDetail"`
+	CityName      string `json:"cityName"`
+	DistrictName  string `json:"districtName"`
+	MallID        int    `json:"mallId"`
+	ProvinceCode  int    `json:"provinceCode"`
+	CityCode      int    `json:"cityCode"`
+	ProvinceName  string `json:"provinceName"`
+	AddressLabel  string `json:"addressLabel"`
 }
