e25bcaad13e358adc825229a8019b5f6265313b6
hiscaler <hiscaler@gmail.com>
2024-09-09 08:58:35 +0800

    ...

--- goods_test.go
+++ goods_test.go
@@ -7,17 +7,18 @@ import (
 	"testing"
 )
 
-func Test_goodsSalesService_BoxMark(t *testing.T) {
-	params := GoodsSalesQueryParams{}
-	items, err := temuClient.Services.GoodsSales.All(ctx, params)
-	assert.Equalf(t, nil, err, "Services.GoodsSales.All(ctx, %s)", jsonx.ToPrettyJson(params))
-
+func Test_goodsService_All(t *testing.T) {
+	params := GoodsQueryParams{}
+	params.PageSize = 2
+	items, err := temuClient.Services.Goods.All(ctx, params)
+	assert.Equalf(t, nil, err, "Services.Goods.All(ctx, %s)", jsonx.ToPrettyJson(params))
+	_ = items
 	if len(items) != 0 {
 		item := items[0]
-		var sales entity.GoodsSales
+		var sales entity.Goods
 		// 根据商品 SKC ID 查询
-		sales, err = temuClient.Services.GoodsSales.One(ctx, item.ProductSkcID)
-		assert.Equalf(t, nil, err, "Services.PurchaseOrder.One(ctx, %d)", item.ProductSkcID)
-		assert.Equalf(t, item, sales, "Services.PurchaseOrder.One(ctx, %d)", item.ProductSkcID)
+		sales, err = temuClient.Services.Goods.One(ctx, item.ProductSkcId)
+		assert.Equalf(t, nil, err, "Services.Goods.One(ctx, %d)", item.ProductSkcId)
+		assert.Equalf(t, item, sales, "Services.Goods.One(ctx, %d)", item.ProductSkcId)
 	}
 }
