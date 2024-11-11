d704ff4de315e782226213066b01f4ea08775308
hiscaler <hiscaler@gmail.com>
2024-11-11 11:14:08 +0800

    ...

--- mall.address.go
+++ mall.address.go
@@ -2,6 +2,7 @@ package temu
 
 import (
 	"context"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/hiscaler/temu-go/entity"
 	"github.com/hiscaler/temu-go/normal"
 )
@@ -45,4 +46,66 @@ func (s mallAddressService) One(ctx context.Context, id int64) (address entity.M
 	}
 
 	return address, ErrNotFound
+}
+
+// 卖家发货地址创建
+
+type CreateDeliveryAddressRequest struct {
+	WarehouseType            int64  `json:"warehouseType"`                      // 仓库类型
+	WarehouseAreaType        string `json:"warehouseAreaType"`                  // 仓库面积类型
+	ProvinceCode             int64  `json:"provinceCode"`                       // 省份编码
+	ProvinceName             string `json:"provinceName"`                       // 省名
+	CityCode                 int64  `json:"cityCode"`                           // 市编码
+	CityName                 string `json:"cityName"`                           // 市名
+	DistrictCode             int64  `json:"districtCode"`                       // 区编码
+	DistrictName             string `json:"districtName"`                       // 区名
+	TownCode                 int64  `json:"townCode,omitempty"`                 // 城镇编码
+	TownName                 string `json:"townName,omitempty"`                 // 城镇
+	ContactPersonName        string `json:"contactPersonName"`                  // 联系人
+	ContactPersonPhoneAreaNo string `json:"contactPersonPhoneAreaNo,omitempty"` // 联系人电话区号
+	ContactPersonPhone       string `json:"contactPersonPhone"`                 // 联系人电话
+	AddressLabel             string `json:"addressLabel"`                       // 地址标签
+	AddressDetail            string `json:"addressDetail"`                      // 详细地址
+}
+
+func (m CreateDeliveryAddressRequest) validate() error {
+	return validation.ValidateStruct(&m,
+		validation.Field(&m.WarehouseType, validation.Required.Error("仓库类型不能为空。")),
+		validation.Field(&m.WarehouseAreaType, validation.Required.Error("仓库面积类型不能为空。")),
+		validation.Field(&m.ProvinceCode, validation.Required.Error("省份编码不能为空。")),
+		validation.Field(&m.ProvinceName, validation.Required.Error("省名不能为空。")),
+		validation.Field(&m.CityCode, validation.Required.Error("市编码不能为空。")),
+		validation.Field(&m.CityName, validation.Required.Error("市名不能为空。")),
+		validation.Field(&m.DistrictCode, validation.Required.Error("区编码不能为空。")),
+		validation.Field(&m.DistrictName, validation.Required.Error("区名不能为空。")),
+		validation.Field(&m.ContactPersonName, validation.Required.Error("联系人不能为空。")),
+		validation.Field(&m.ContactPersonPhone, validation.Required.Error("联系人电话不能为空。")),
+		validation.Field(&m.AddressLabel, validation.Required.Error("地址标签不能为空。")),
+		validation.Field(&m.AddressDetail, validation.Required.Error("详细地址不能为空。")),
+	)
+}
+
+func (s mallAddressService) Create(ctx context.Context, request CreateDeliveryAddressRequest) (addressId int64, err error) {
+	if err = request.validate(); err != nil {
+		return
+	}
+	var result = struct {
+		normal.Response
+		Result struct {
+			AddressId int64 `json:"addressId"`
+		} `json:"result"`
+	}{}
+	resp, err := s.httpClient.R().
+		SetContext(ctx).
+		SetBody(request).
+		SetResult(&result).
+		Post("bg.mall.address.add")
+	if err == nil {
+		err = parseResponse(resp, result.Response)
+	}
+	if err != nil {
+		return
+	}
+
+	return result.Result.AddressId, nil
 }
