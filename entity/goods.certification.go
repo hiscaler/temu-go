ab47f2353870561873431e00ad648665ddc05b7c
hiscaler <hiscaler@gmail.com>
2024-11-13 18:09:03 +0800

    ...

--- entity/goods.certification.go
+++ entity/goods.certification.go
@@ -9,3 +9,33 @@ type GoodsCertification struct {
 	UpdateReason string `json:"updateReason"` // 更新原因
 	AuditStatus  int    `json:"auditStatus"`  // 审核状态
 }
+
+// GoodsCertificationNeedUploadItem 资质要上传的内容
+type GoodsCertificationNeedUploadItem struct {
+	AliasName string `json:"aliasName"` // 文件名称
+	// 当前资质类型需上传文件类型，枚举1-4，明细如下。
+	// 返回多个时，”提交资质内容“时相关字段均需要上传。
+	// - contentType：1  含义：资质证书
+	//  - 必传字段：productCertFiles-货品资质文件列表
+	// - contentType：2  含义：检测报告
+	//  - 必传字段：inspectReportFiles-检测报告文件列表
+	// - contentType：3  含义：资质编号
+	//  - 必传字段：authCode-资质编号
+	// - contentType：4  含义：产品实拍图
+	//  - 使用“bg.flash.open.upload.real.image”上传
+	//  - 必传字段：realPictures-实物图列表
+	ContentType int `json:"contentType"` // 文件类型
+	// hasExpireTime = true时，提交接口中的expireTime需不为null
+	HasExpireTime bool `json:"hasExpireTime"` // 资质是否存在有效期
+	ExpireDays    int  `json:"expireDays"`    // 固定失效天数
+	// -needShowCustomer返回“true”时
+	// bg.arbok.open.cert.uploadProductCert，需要入参showCustomer
+	NeedShowCustomer bool `json:"needShowCustomer"` // 是否需要c端展示
+	// 文件失效形式
+	// 对不同类型资质的不同类型文件采取不同的管控方式，有固定日期，固定时长，不固定三种
+	// - 1：不固定方式：失效日期不为null
+	// - 2：固定日期方式：失效日期不为null
+	// - 3：固定时长方式：失效日期不为null且生效日期不为null
+	ExpireTime       int `json:"expireTime"`       // 固定失效日期
+	ExpireNoticeDays int `json:"expireNoticeDays"` // 提前预警天数
+}
--- goods.certification.go
+++ goods.certification.go
@@ -1,6 +1,10 @@
 package temu
 
-import "github.com/hiscaler/temu-go/entity"
+import (
+	"context"
+	"github.com/hiscaler/temu-go/entity"
+	"github.com/hiscaler/temu-go/normal"
+)
 
 type goodsCertificationService service
 
@@ -10,6 +14,68 @@ type GoodsCertificationQueryRequest struct {
 	Language     string `json:"language"`     // 语言
 }
 
-func (s goodsCertificationService) Query(request GoodsCertificationQueryRequest) (certification []entity.GoodsCertification, err error) {
+func (m GoodsCertificationQueryRequest) validate() error {
+	return nil
+}
+
+// Query 批量查询货品资质信息
+// https://seller.kuajingmaihuo.com/sop/view/649320516224723675#Oq8dC9
+func (s goodsCertificationService) Query(ctx context.Context, request GoodsCertificationQueryRequest) (certifications []entity.GoodsCertification, err error) {
+	if err = request.validate(); err != nil {
+		return
+	}
+	var result = struct {
+		normal.Response
+		Result struct {
+			ProductCertList []entity.GoodsCertification `json:"productCertList"`
+		} `json:"result"`
+	}{}
+	resp, err := s.httpClient.R().
+		SetContext(ctx).
+		SetBody(request).
+		SetResult(&result).
+		Post("bg.arbok.open.product.cert.query")
+	if err == nil {
+		err = parseResponse(resp, result.Response)
+	}
+	if err != nil {
+		return
+	}
+	certifications = result.Result.ProductCertList
+	return
+}
+
+type GoodsCertificationNeedUploadItemRequest struct {
+	CertType  int   `json:"certType"`  // 资质类型
+	ProductId int64 `json:"productId"` // 货品id
+}
+
+func (m GoodsCertificationNeedUploadItemRequest) validate() error {
+	return nil
+}
+
+func (s goodsCertificationService) QueryNeedUploadItems(ctx context.Context, request GoodsCertificationNeedUploadItemRequest) (items []entity.GoodsCertificationNeedUploadItem, err error) {
+	if err = request.validate(); err != nil {
+		return
+	}
+	var result = struct {
+		normal.Response
+		Result struct {
+			CertNeedUploadItems []entity.GoodsCertificationNeedUploadItem `json:"certNeedUploadItems"`
+		} `json:"result"`
+	}{}
+	resp, err := s.httpClient.R().
+		SetContext(ctx).
+		SetBody(request).
+		SetResult(&result).
+		Post("bg.arbok.open.cert.queryNeedUploadItems")
+	if err == nil {
+		err = parseResponse(resp, result.Response)
+	}
+	if err != nil {
+		return
+	}
+
+	items = result.Result.CertNeedUploadItems
 	return
 }
