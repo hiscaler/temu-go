c2fffcfcf0911ba19da3fbf99f295e7331149572
hiscaler <hiscaler@gmail.com>
2024-12-03 11:39:23 +0800

    ...

--- entity/goods.life.cycle.go
+++ entity/goods.life.cycle.go
@@ -6,9 +6,9 @@ type GoodsLifeCycle struct {
 		SelectStatus int   `json:"selectStatus"` // 选品状态
 		SkuList      struct {
 			SkuId int64 `json:"skuId"`
-		} `json:"skuList"` // sku列表
-		ApplyJitStatus  int  // 申诉JIT的状态(1-可申请；3-不可申请)
-		SuggestCloseJit bool `json:"suggestCloseJit"` // 是否建议关闭JIT按钮
+		} `json:"skuList"` // sku 列表
+		ApplyJitStatus  int  // 申诉 JIT 的状态(1：可申请、3：不可申请)
+		SuggestCloseJit bool `json:"suggestCloseJit"` // 是否建议关闭 JIT 按钮
 	} `json:"skcList"` // SKC 列表
-	ProductId int64 `json:"productId"` // 货品 id
+	ProductId int64 `json:"productId"` // 货品 ID
 }
