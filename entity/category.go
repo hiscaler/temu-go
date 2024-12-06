4e9983034879f4bb685e624e3575d51495ff6df0
hiscaler <hiscaler@gmail.com>
2024-12-06 10:50:29 +0800

    ...

--- entity/category.go
+++ entity/category.go
@@ -4,3 +4,14 @@ type SimpleCategory struct {
 	CatId   int64  `json:"catId"`   // 类目 ID
 	CatName string `json:"catName"` // 类目名称
 }
+
+type Category struct {
+	CatID       int64  `json:"catId"`
+	CatName     string `json:"catName"`
+	CatType     int    `json:"catType"` //  1 是服饰，其他的非服饰
+	CatLevel    int    `json:"catLevel"`
+	ParentCatID int64  `json:"parentCatId"`
+	IsLeaf      bool   `json:"isLeaf"` // true=叶子类目
+	IsHidden    bool   `json:"isHidden"`
+	HiddenType  int    `json:"hiddenType"`
+}
