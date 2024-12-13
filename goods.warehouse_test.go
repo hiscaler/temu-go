8151df71c8194f8914985462f5b66ae593e83390
hiscaler <hiscaler@gmail.com>
2024-12-13 11:54:11 +0800

    ...

--- goods.warehouse_test.go
+++ goods.warehouse_test.go
@@ -1,20 +1,13 @@
 package temu
 
 import (
+	"github.com/hiscaler/gox/jsonx"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_goodsWarehouseService_Query(t *testing.T) {
-	// params := GoodsSalesQueryParams{}
-	_ = temuClient.Services.Goods.Warehouse.Query(ctx)
-	// assert.Equalf(t, nil, err, "Services.GoodsSales.Query(ctx, %s)", jsonx.ToPrettyJson(params))
-	//
-	// if len(items) != 0 {
-	// 	item := items[0]
-	// 	var sales entity.GoodsSales
-	// 	// 根据商品 SKC ID 查询
-	// 	sales, err = temuClient.Services.Goods.Sales.One(ctx, item.ProductSkcId)
-	// 	assert.Equalf(t, nil, err, "Services.GoodsSales.One(ctx, %d)", item.ProductSkcId)
-	// 	assert.Equalf(t, item, sales, "Services.GoodsSales.One(ctx, %d)", item.ProductSkcId)
-	// }
+	params := GoodsWarehouseQueryParams{SiteIdList: []int64{6545025}}
+	err := temuClient.Services.Goods.Warehouse.Query(ctx, params)
+	assert.Equalf(t, nil, err, "Services.Goods.Warehouse.Query(ctx, %s)", jsonx.ToPrettyJson(params))
 }
