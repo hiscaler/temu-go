5c7e11f5e46d6364a423c4f7189009dd200e282b
hiscaler <hiscaler@gmail.com>
2024-12-14 09:42:30 +0800

    ...

--- normal/response.go
+++ normal/response.go
@@ -1,9 +1,9 @@
 package normal
 
 type Response struct {
-	Success      bool   `json:"success"`
-	RequestId    string `json:"requestId"`
-	ErrorCode    int    `json:"errorCode"`
-	ErrorMessage string `json:"errorMsg"`
-	Result       any    `json:"result"`
+	Success      bool   `json:"success"`   // 是否成功
+	RequestId    string `json:"requestId"` // 请求 ID
+	ErrorCode    int    `json:"errorCode"` // 错误码
+	ErrorMessage string `json:"errorMsg"`  // 错误信息
+	Result       any    `json:"result"`    // 返回结果
 }
