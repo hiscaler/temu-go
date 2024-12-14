93f3b329a8565ac5bbf0aaa4b39e594a062b7ae6
hiscaler <hiscaler@gmail.com>
2024-12-14 09:39:35 +0800

    ...

--- ship.order.receive.address.go
+++ ship.order.receive.address.go
@@ -2,7 +2,6 @@ package temu
 
 import (
 	"context"
-	"errors"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/hiscaler/temu-go/entity"
 	"github.com/hiscaler/temu-go/normal"
@@ -15,16 +14,13 @@ type shipOrderReceiveAddressService service
 
 // Query 查询大仓收货地址 V2
 // https://seller.kuajingmaihuo.com/sop/view/889973754324016047#chUUk1
-func (s shipOrderReceiveAddressService) Query(ctx context.Context, subPurchaseOrderSnList ...string) (items []entity.ShipOrderReceiveAddress, err error) {
-	if len(subPurchaseOrderSnList) == 0 {
-		err = errors.New("备货单号不能为空")
-		return
-	}
-
-	err = validation.Validate(subPurchaseOrderSnList, validation.Each(validation.By(is.PurchaseOrderNumber())))
+func (s shipOrderReceiveAddressService) Query(ctx context.Context, purchaseOrderNumbers ...string) (items []entity.ShipOrderReceiveAddress, err error) {
+	err = validation.Validate(purchaseOrderNumbers,
+		validation.Required.Error("备货单号不能为空"),
+		validation.Each(validation.By(is.PurchaseOrderNumber())),
+	)
 	if err != nil {
-		err = invalidInput(err)
-		return
+		return items, invalidInput(err)
 	}
 
 	var result = struct {
@@ -35,7 +31,7 @@ func (s shipOrderReceiveAddressService) Query(ctx context.Context, subPurchaseOr
 	}{}
 	resp, err := s.httpClient.R().
 		SetContext(ctx).
-		SetBody(map[string][]string{"subPurchaseOrderSnList": subPurchaseOrderSnList}).
+		SetBody(map[string][]string{"subPurchaseOrderSnList": purchaseOrderNumbers}).
 		SetResult(&result).
 		Post("bg.shiporder.receiveaddressv2.get")
 	if err = recheckError(resp, result.Response, err); err != nil {
@@ -46,15 +42,15 @@ func (s shipOrderReceiveAddressService) Query(ctx context.Context, subPurchaseOr
 }
 
 // One [WIP] 查询单个备货单收货地址
-func (s shipOrderReceiveAddressService) One(ctx context.Context, subPurchaseOrderSn string) (item entity.ShipOrderReceiveAddress, err error) {
-	items, err := s.Query(ctx, subPurchaseOrderSn)
+func (s shipOrderReceiveAddressService) One(ctx context.Context, purchaseOrderNumber string) (item entity.ShipOrderReceiveAddress, err error) {
+	items, err := s.Query(ctx, purchaseOrderNumber)
 	if err != nil {
 		return
 	}
 
 	for _, d := range items {
 		for _, sn := range d.SubPurchaseOrderSnList {
-			if strings.EqualFold(sn, subPurchaseOrderSn) {
+			if strings.EqualFold(sn, purchaseOrderNumber) {
 				return d, nil
 			}
 		}
