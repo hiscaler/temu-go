199d4c2a43f922a21ee02bb420f01b9190f4d495
hiscaler <hiscaler@gmail.com>
2024-12-16 09:36:22 +0800

    ...

--- entity/logistics.company.go
+++ entity/logistics.company.go
@@ -2,12 +2,12 @@ package entity
 
 // LogisticsShippingCompany 发货快递公司
 type LogisticsShippingCompany struct {
-	ShipId   int    `json:"shipId"`   // 快递公司 ID
+	ShipId   int64  `json:"shipId"`   // 快递公司 ID
 	ShipName string `json:"shipName"` // 快递公司名称
 }
 
 // LogisticsExpressCompany 物流快递公司
 type LogisticsExpressCompany struct {
-	ExpressCompanyId   int    `json:"expressCompanyId"`   // 快递公司 ID
+	ExpressCompanyId   int64  `json:"expressCompanyId"`   // 快递公司 ID
 	ExpressCompanyName string `json:"expressCompanyName"` // 快递公司名称
 }
