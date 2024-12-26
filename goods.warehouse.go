09d3059377961e23da7b37628a5531eb5514a4c2
hiscaler <hiscaler@gmail.com>
2024-12-26 19:44:12 +0800

    ...

--- goods.warehouse.go
+++ goods.warehouse.go
@@ -2,6 +2,7 @@ package temu
 
 import (
 	"context"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/hiscaler/temu-go/entity"
 	"github.com/hiscaler/temu-go/normal"
 )
@@ -14,16 +15,18 @@ type GoodsWarehouseQueryParams struct {
 }
 
 func (m GoodsWarehouseQueryParams) validate() error {
-	return nil
+	return validation.ValidateStruct(&m,
+		validation.Field(&m.SiteIdList, validation.Required.Error("站点列表不能为空")),
+	)
 }
 
 // Query 根据站点查询可绑定的发货仓库信息接口
 // https://seller.kuajingmaihuo.com/sop/view/867739977041685428#hpIdAo
 func (s goodsWarehouseService) Query(ctx context.Context, params GoodsWarehouseQueryParams) (items []entity.SiteWarehouses, err error) {
 	if err = params.validate(); err != nil {
-		err = invalidInput(err)
-		return
+		return items, invalidInput(err)
 	}
+
 	var result = struct {
 		normal.Response
 		Result struct {
