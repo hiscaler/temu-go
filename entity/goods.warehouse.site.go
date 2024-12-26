e4d0b527bb3d993b8cdb11f9351fbde4fed59624
hiscaler <hiscaler@gmail.com>
2024-12-26 19:45:15 +0800

    ...

--- entity/goods.warehouse.site.go
+++ entity/goods.warehouse.site.go
@@ -1,11 +1,11 @@
 package entity
 
 type SiteWarehouses struct {
-	SiteId             int    `json:"siteId"`   // 站点 id
+	SiteId             int    `json:"siteId"`   // 站点 Id
 	SiteName           string `json:"siteName"` // 站点名称
 	ValidWarehouseList []struct {
 		WarehouseId      string `json:"warehouseId"`
 		WarehouseName    string `json:"warehouseName"`
 		WarehouseDisable bool   `json:"warehouseDisable"`
-	} `json:"validWarehouseList"` //
+	} `json:"validWarehouseList"` // 有效仓库列表
 }
