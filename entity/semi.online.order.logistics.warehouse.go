798b0e0be6bb6ae3689e32c88418a822118239ac
hiscaler <hiscaler@gmail.com>
2025-01-15 15:32:50 +0800

    ...

--- entity/semi.online.order.logistics.warehouse.go
+++ entity/semi.online.order.logistics.warehouse.go
@@ -1,7 +1,7 @@
 package entity
 
 type SemiOnlineOrderLogisticsWarehouse struct {
-	WarehouseId      int64  `json:"warehouseId"`      // 仓库 Id
+	WarehouseId      string `json:"warehouseId"`      // 仓库 Id
 	WarehouseName    string `json:"warehouseName"`    // 仓库名称
 	RegionId         int    `json:"regionId"`         // 所属经营站点
 	DefaultWarehouse bool   `json:"defaultWarehouse"` // 是否默认仓库
