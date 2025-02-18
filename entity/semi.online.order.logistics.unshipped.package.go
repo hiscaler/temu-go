00f24df059bb05f10fe8846a0c68ef8db138a667
hiscaler <hiscaler@gmail.com>
2025-02-18 15:58:52 +0800

    ...

--- entity/semi.online.order.logistics.unshipped.package.go
+++ entity/semi.online.order.logistics.unshipped.package.go
@@ -1,9 +1,8 @@
 package entity
 
 type SemiOnlineOrderPlatformLogisticsUnshippedPackage struct {
-	CarrierId      int64  `json:"carrierId"`      // 物流公司 ID
-	CarrierName    string `json:"carrierName"`    // 物流公司名称
-	TrackingNumber string `json:"trackingNumber"` // 运单号
+	CarrierId   int64  `json:"carrierId"`   // 物流渠道 ID
+	CarrierName string `json:"carrierName"` // 物流渠道名称
 	// 对应枚举如下：
 	//	主包裹：MAIN
 	//	子包裹：SPLIT_LARGE_ITEM
@@ -12,6 +11,8 @@ type SemiOnlineOrderPlatformLogisticsUnshippedPackage struct {
 	//	2、默认所有非单sku多包裹场景的包裹都是主包裹。
 	SubPackageType   string `json:"subPackageType"` // 包裹主子类型
 	MainPackageSn    string `json:"mainPackageSn"`  // 该包裹关联的主包裹。当包裹为主包裹时返回自身，当包裹为子包裹时返回关联的主包裹
+	PackageSn        string `json:"packageSn"`      // 包裹号
+	TrackingNumber   string `json:"trackingNumber"` // 运单号
 	SubPackageSnList []struct {
 		ShippableOrders []struct {
 			ParentOrderSn string `json:"parentOrderSn"` // 父单号
@@ -24,4 +25,12 @@ type SemiOnlineOrderPlatformLogisticsUnshippedPackage struct {
 			Quantity      int    `json:"quantity"`      // 应履约件数
 		} `json:"canceledOrders"` // 包裹内已被取消的O单详情
 	} `json:"subPackageSnList"` // 该包裹下的子包裹列表。当包裹为主包裹时返回关联的子包裹，当包裹为子包裹时返回为空。
+	PackageDetail struct {
+		CanceledOrders  []any `json:"canceledOrders"`
+		ShippableOrders []struct {
+			ParentOrderSn string `json:"parentOrderSn"`
+			OrderSn       string `json:"orderSn"`
+			Quantity      int    `json:"quantity"`
+		} `json:"shippableOrders"`
+	} `json:"packageDetail"` // 包裹详情
 }
