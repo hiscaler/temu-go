ca9904829c397d99875b94923a5d549e2a7aa1c6
hiscaler <hiscaler@gmail.com>
2025-03-13 10:42:52 +0800

    New:  添加半托订单发货通知接口

--- semi.order.logistics.shipment.go
+++ semi.order.logistics.shipment.go
@@ -48,3 +48,57 @@ func (s *semiOrderLogisticsShipmentService) Query(ctx context.Context, params Se
 
 	return result.Result.ShipmentInfoDTO, nil
 }
+
+// 订单发货通知
+
+type SemiOrderLogisticsShipmentConfirmInformationOrder struct {
+	OrderSn       string `json:"orderSn"`       // orderSn
+	ParentOrderSn string `json:"parentOrderSn"` // parentOrderSn
+	GoodsId       int64  `json:"goodsId"`       // goodsId
+	SkuId         int64  `json:"skuId"`         // skuId
+	Quantity      int    `json:"quantity"`      // 发货数量
+}
+type SemiOrderLogisticsShipmentConfirmInformation struct {
+	CarrierId         int64                                               `json:"carrierId"`         // 物流公司 ID
+	TrackingNumber    string                                              `json:"trackingNumber"`    // 运单号
+	OrderSendInfoList []SemiOrderLogisticsShipmentConfirmInformationOrder `json:"orderSendInfoList"` // 发货商品信息
+}
+
+type SemiOrderLogisticsShipmentConfirmRequest struct {
+	SendType        int                                            `json:"sendType"`        // 发货类型：0-单个运单发货 1-拆成多个运单发货 2-合并发货
+	SendRequestList []SemiOrderLogisticsShipmentConfirmInformation `json:"sendRequestList"` // 包裹信息
+}
+
+func (m SemiOrderLogisticsShipmentConfirmRequest) validate() error {
+	return validation.ValidateStruct(&m,
+		validation.Field(&m.SendType, validation.In(0, 1, 2).Error("无效的发货类型")),
+		validation.Field(&m.SendRequestList, validation.Required.Error("发货商品信息不能为空")),
+	)
+}
+
+// Confirm 订单发货通知接口（bg.logistics.shipment.confirm）
+// https://seller.kuajingmaihuo.com/sop/view/867739977041685428#bCMdFx
+func (s *semiOrderLogisticsShipmentService) Confirm(ctx context.Context, request SemiOrderLogisticsShipmentConfirmRequest) (bool, error) {
+	if err := request.validate(); err != nil {
+		return false, err
+	}
+
+	var result = struct {
+		normal.Response
+		Result struct {
+			AssistantAgreementText string   `json:"assistantAgreementText"` // 智能助手协议文案，当发货时，默认发货成功即为确认同意开启智能轨迹助手
+			WarningMessage         []string `json:"warningMessage"`         // 发货操作提醒字段
+		} `json:"result"`
+	}{}
+	resp, err := s.httpClient.R().
+		SetContext(ctx).
+		SetBody(request).
+		SetResult(&result).
+		Post("bg.logistics.shipment.confirm")
+
+	if err = recheckError(resp, result.Response, err); err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
