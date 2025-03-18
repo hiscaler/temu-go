80be1ca2e2d57865d0916b5a8fe3e80368636def
hiscaler <hiscaler@gmail.com>
2025-03-18 08:57:46 +0800

    ...

--- goods.category.attribute.go
+++ goods.category.attribute.go
@@ -11,6 +11,7 @@ import (
 type goodsCategoryAttributeService service
 
 // Query 按类目查询货品属性
+// https://seller.kuajingmaihuo.com/sop/view/728777295758127187#6bz75P
 func (s goodsCategoryAttributeService) Query(ctx context.Context, categoryId int64) ([]entity.GoodsCategoryAttribute, error) {
 	if categoryId <= 0 {
 		return nil, fmt.Errorf("无效的分类：%d", categoryId)
