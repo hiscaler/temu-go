cd6059c79506636d68e5780478c441d3485a6bc2
hiscaler <hiscaler@gmail.com>
2025-03-19 16:09:33 +0800

    New: 添加物流模版查询接口

--- semi.logistics.go
+++ semi.logistics.go
@@ -2,6 +2,7 @@ package temu
 
 import (
 	"context"
+	"errors"
 	"github.com/hiscaler/temu-go/entity"
 	"github.com/hiscaler/temu-go/normal"
 )
@@ -28,4 +29,29 @@ func (s semiLogisticsService) Companies(ctx context.Context, regionId int) ([]en
 	}
 
 	return result.Result.Items, nil
+}
+
+// Template 物流模版查询
+// https://seller.kuajingmaihuo.com/sop/view/867739977041685428#pa858C
+func (s semiLogisticsService) Template(ctx context.Context, siteIds ...int) ([]entity.FreightTemplate, error) {
+	if len(siteIds) == 0 {
+		return nil, errors.New("请提供站点 ID")
+	}
+
+	var result = struct {
+		normal.Response
+		Result struct {
+			FreightTemplates []entity.FreightTemplate `json:"freightTemplates"`
+		} `json:"result"`
+	}{}
+	resp, err := s.httpClient.R().
+		SetContext(ctx).
+		SetBody(map[string][]int{"siteIds": siteIds}).
+		SetResult(&result).
+		Post("bg.logistics.template.get")
+	if err = recheckError(resp, result.Response, err); err != nil {
+		return nil, err
+	}
+
+	return result.Result.FreightTemplates, nil
 }
