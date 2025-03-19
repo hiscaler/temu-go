8f6756a5641c40b48c55846e81706f27859a0388
hiscaler <hiscaler@gmail.com>
2025-03-19 11:36:58 +0800

    New: 添加 is.ImageUrl 验证规则

--- validators/is/is.go
+++ validators/is/is.go
@@ -9,7 +9,8 @@ var millisecondPattern,
 	shipOrderNumberPattern,
 	mobilePhoneNumberPattern,
 	telNumberPattern,
-	telNumberAreaCodePattern *regexp.Regexp
+	telNumberAreaCodePattern,
+	imageUrlPattern *regexp.Regexp
 
 func init() {
 	millisecondPattern = regexp.MustCompile(`^[1-9][0-9]{12}$`)
@@ -20,4 +21,5 @@ func init() {
 	mobilePhoneNumberPattern = regexp.MustCompile(`^(?:\+?86)?1(?:3\d{3}|5[^4\D]\d{2}|8\d{3}|7(?:[0-35-9]\d{2}|4(?:0\d|1[0-2]|9\d))|9[0-35-9]\d{2}|6[2567]\d{2}|4(?:(?:10|4[01])\d{3}|[68]\d{4}|[579]\d{2}))\d{6}$`)
 	telNumberPattern = regexp.MustCompile(`^(0\d{2,3}-?)?\d{8}$`)
 	telNumberAreaCodePattern = regexp.MustCompile(`^0(\d{2,3})$`)
+	imageUrlPattern = regexp.MustCompile(`^https?://.*\.(?:png|jpg|jpeg)(?:\?.*)?$`)
 }
