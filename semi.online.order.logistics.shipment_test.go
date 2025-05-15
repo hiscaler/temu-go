882f5a4ae84ea014c773ab09101e4547f4950c9d
hiscaler <hiscaler@gmail.com>
2025-05-15 13:41:06 +0800

    ...

--- semi.online.order.logistics.shipment_test.go
+++ semi.online.order.logistics.shipment_test.go
@@ -3,6 +3,7 @@ package temu
 import (
 	"context"
 	"fmt"
+	"github.com/hiscaler/temu-go/entity"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -14,20 +15,29 @@ func Test_semiOnlineOrderLogisticsShipmentService_Query(t *testing.T) {
 	}
 	tests := []struct {
 		name    string
-		s       semiOnlineOrderLogisticsShipmentService
 		args    args
 		want    []entity.SemiOnlineOrderLogisticsShipmentPackageResult
 		wantErr assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{
+				ctx:            ctx,
+				packageNumbers: []string{"PK-5265463056138373225"},
+			},
+			want: []entity.SemiOnlineOrderLogisticsShipmentPackageResult{},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				return assert.NoError(t, err, i)
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.s.Query(tt.args.ctx, tt.args.packageNumbers...)
-			if !tt.wantErr(t, err, fmt.Sprintf("Query(%v, %v)", tt.args.ctx, tt.args.packageNumbers...)) {
+			got, err := temuClient.Services.SemiManaged.OnlineOrder.Logistics.Shipment.Query(ctx, tt.args.packageNumbers...)
+			if !tt.wantErr(t, err, fmt.Sprintf("Query(%v, %v)", tt.args.ctx, tt.args.packageNumbers)) {
 				return
 			}
-			assert.Equalf(t, tt.want, got, "Query(%v, %v)", tt.args.ctx, tt.args.packageNumbers...)
+			assert.Equalf(t, tt.want, got, "Query(%v, %v)", tt.args.ctx, tt.args.packageNumbers)
 		})
 	}
 }
