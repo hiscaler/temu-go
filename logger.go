13a9051674b286f6374a3ca7c50bb8d1e2e3cda9
hiscaler <hiscaler@gmail.com>
2025-07-22 16:41:16 +0800

    Bug: 修正自定义日志不契合 Resty 日志的问题

--- logger.go
+++ logger.go
@@ -1,7 +1,9 @@
 package temu
 
 import (
+	"fmt"
 	"log/slog"
+	"strings"
 )
 
 type Logger interface {
@@ -23,17 +25,33 @@ type logger struct {
 var _ Logger = (*logger)(nil)
 
 func (l *logger) Errorf(msg string, args ...interface{}) {
+	if strings.Contains(msg, "%") {
+		l.l.Error(fmt.Sprintf(msg, args...))
+		return
+	}
 	l.l.Error(msg, args...)
 }
 
 func (l *logger) Warnf(msg string, args ...interface{}) {
+	if strings.Contains(msg, "%") {
+		l.l.Warn(fmt.Sprintf(msg, args...))
+		return
+	}
 	l.l.Warn(msg, args...)
 }
 
 func (l *logger) Infof(msg string, args ...interface{}) {
+	if strings.Contains(msg, "%") {
+		l.l.Info(fmt.Sprintf(msg, args...))
+		return
+	}
 	l.l.Info(msg, args...)
 }
 
 func (l *logger) Debugf(msg string, args ...interface{}) {
+	if strings.Contains(msg, "%") {
+		l.l.Debug(fmt.Sprintf(msg, args...))
+		return
+	}
 	l.l.Debug(msg, args...)
 }
