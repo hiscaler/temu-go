6c159f097f12e5fbcaaf4545a7c6153d5adb2784
hiscaler <hiscaler@gmail.com>
2026-03-21 09:48:57 +0800

    New: 添加 Labels.Is 方法测试

--- semi.order_test.go
+++ semi.order_test.go
@@ -3,6 +3,7 @@ package temu
 import (
 	"testing"
 
+	"github.com/hiscaler/temu-go/entity"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,4 +18,23 @@ func TestOrder(t *testing.T) {
 	items, total, totalPage, isLatestPage, err := temuClient.Services.SemiManaged.Order.Query(ctx, params)
 	assert.Equalf(t, nil, err, "Services.SemiManaged.Order.Query(ctx, %#v) err", params)
 	println(items, total, totalPage, isLatestPage)
+}
+// TestIs 测试标签是否包含指定标签
+func TestIs(t *testing.T) {
+	labels := entity.Labels{
+		entity.Label{
+			Name:  "A",
+			Value: 1,
+		},
+		entity.Label{
+			Name:  "B",
+			Value: 0,
+		},
+		entity.Label{
+			Name:  "C",
+			Value: 1,
+		},
+	}
+	assert.Equalf(t, true, labels.Is("A", "C"), "Is(%v, %v)", "A", "C")
+	assert.Equalf(t, false, labels.Is("A", "B"), "Is(%v, %v)", "A", "B")
 }
